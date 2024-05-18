9d24baae84ef43f61e8b589e8af5d58f960c31e6
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-05-18 14:31:07 +0700

    Layout setting

---
+++ src/pages/BookCraftingPage.tsx
@@ -0,0 +1,7 @@
+const BookCraftingPage = () => {
+  return (
+    <div>BookCraftingPage</div>
+  )
+}
+
+export default BookCraftingPage
---
+++ src/routers/AuthRouter.tsx
@@ -0,0 +1,22 @@
+import AuthLayout from "@/layouts/AuthLayout";
+import LoginPage from "@/pages/LoginPage";
+import RegisterPage from "@/pages/RegisterPage";
+import { RouteObject } from "react-router-dom";
+
+const AuthRouter :  RouteObject[] = [
+    {
+        path:'/auth',
+        element: <AuthLayout/>,
+        children: [
+            {
+                path: '/login',
+                element: <LoginPage/>
+            },{
+                path: '/register',
+                element: <RegisterPage/>
+            }
+        ]
+    }
+]
+
+export default AuthRouter;
