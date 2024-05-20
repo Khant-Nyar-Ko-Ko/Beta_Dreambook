69036c3e98ff1e34a534bbbed60de3918bbdea1f
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-05-21 00:34:11 +0700

    complete navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,27 @@
+import logo from "../assets/images/Logo.svg";
+import { IoPersonCircle } from "react-icons/io5";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <div>This is Navbar</div>
-  )
-}
+    <div className="w-screen h-[80px] flex justify-between items-center px-[120px] bg-white shadow">
+      <img src={logo} className=" w-[75px] h-[61px]" alt="" />
+      <div className="flex gap-2  w-[375px] h-[40px]">
+        <Button className=" text-black hover:text-white bg-transparent hover:bg-[#3A7AD5] px-4 py-1 rounded-full duration-100">Home</Button>
+        <Button className=" text-black hover:text-white bg-transparent hover:bg-[#3A7AD5] px-4 py-1 rounded-full duration-100">Library</Button>
+        <Button className=" text-black hover:text-white bg-transparent hover:bg-[#3A7AD5] px-4 py-1 rounded-full duration-100">Book crafting</Button>
+      </div>
+      <div className="flex gap-[10px] items-center">
+        <Button className=" flex gap-2 text-[#3A7AD5] hover:text-white hover:bg-[#3A7AD5] px-4 py-2 rounded-full duration-100">
+          <IoPersonCircle className="w-6 h-6 rounded" />
+          Login
+        </Button>
+        <Button className="text-[#3A7AD5] hover:text-white hover:bg-[#3A7AD5] px-4 py-2 rounded-full duration-100">
+          Register
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
