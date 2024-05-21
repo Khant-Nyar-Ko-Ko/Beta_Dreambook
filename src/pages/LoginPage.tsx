e78cce8668451189ec60a77cdd12c2a8e21f4f47
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-05-21 14:18:43 +0700

    finish login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,50 @@
+import { Input } from "@/components/ui/input";
+import background from "../assets/images/AuthBgImage.avif";
+import logo from "../assets/images/Login/Vector 2.svg";
+import { Button } from "@/components/ui/button";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className="relative w-screen h-screen">
+    {/* background */}
+    <img
+      src={background}
+      className="absolute top-0 left-0 object-cover w-full h-full"
+      alt="Background"
+    />
+    <div className="absolute top-0 left-0 w-full h-full bg-[#022557] opacity-80 z-10"></div>
+    <div className="absolute top-0 left-0 z-20 flex items-center justify-center w-full h-full ">
+      <div className="flex flex-col items-center gap-9">
+        {/* logo */}
+        <div className=" mb-7">
+        <div className="flex gap-4 w-[370px]">
+          <img src={logo} className="w-20 h-16 " alt="" />
+          <div className="w-[350px] flex flex-col gap-1">
+            <h3 className="text-xl font-bold text-white">Dream Book</h3>
+            <p className="text-sm text-white ">
+              Book Reading & Publishing Platform
+            </p>
+          </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <h2 className="text-3xl text-white ">Welcome again!</h2>
+          <p className="text-white">Please login to your account</p>
+        </div>
+        <div className="flex flex-col text-center gap-7">
+          <div className="flex flex-col gap-8 ">
+            <Input placeholder="Username"/>
+            <Input inputType="password" placeholder="Password" />
+          </div>
+          <Button>Log in</Button>
+          <p className="text-white ">
+            Don't have an account? 
+            <span className="font-semibold "> Create an acccount</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
