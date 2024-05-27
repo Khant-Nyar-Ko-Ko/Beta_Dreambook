35fee54141c3e6ac14697c85f550c254a67fd7bd
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-05-27 16:22:23 +0700

    link

--- src/components/ViewMore.tsx
+++ src/components/ViewMore.tsx
@@ -1,15 +1,18 @@
 import { IoIosArrowForward } from "react-icons/io";
 import { Button } from "./ui/button";
+import { NavLink } from "react-router-dom";
 
 const ViewMore = () => {
   return (
     <>
-      <Button
-        variant="white"
-        className="flex items-center gap-2 text-center text-black hover:text-black hover:bg-transparent"
-      >
-        View More <IoIosArrowForward />
-      </Button>
+      <NavLink to={"/library"}>
+        <Button
+          variant="white"
+          className="flex items-center gap-2 text-center text-black hover:text-black hover:bg-transparent"
+        >
+          View More <IoIosArrowForward />
+        </Button>
+      </NavLink>
     </>
   );
 };
