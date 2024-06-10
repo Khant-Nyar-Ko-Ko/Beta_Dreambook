6562c919427453fbfa570fe1fc2176930179bacd
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-06-10 11:33:57 +0700

    validation, loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <>
+      <div className="w-5 h-5 border-4 border-white border-opacity-75 rounded-full border-t-transparent animate-spin"></div>
+    </>
+  );
+};
+
+export default Loading;
