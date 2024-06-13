20406ade7c8c06c6d6f08933ea2d91ca50617a79
Valyr1c <kaungthux@gmail.com>
2024-06-13 09:21:39 +0630

    changes

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,101 +1,41 @@
-import "@blocknote/core/fonts/inter.css";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
-import {
-  BasicTextStyleButton,
-  BlockTypeSelect,
-  ColorStyleButton,
-  CreateLinkButton,
-  FileCaptionButton,
-  FileReplaceButton,
-  FormattingToolbar,
-  FormattingToolbarController,
-  NestBlockButton,
-  TextAlignButton,
-  UnnestBlockButton,
-  useCreateBlockNote,
-} from "@blocknote/react";
+import React, { useRef } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-import { BlueButton } from "./BlueButton";
-
-export default function App() {
-  // Creates a new editor instance.
-  const editor = useCreateBlockNote({
-    initialContent: [
-
-      {
-        type: "paragraph",
-        content: "Write your Book Decription",
-      },
-      {
-        type: "paragraph",
-      },
-    ],
-  });
-
-  // Renders the editor instance.
-  return (
-    <BlockNoteView
-      editor={editor}
-      formattingToolbar={false}
-      theme={"light"}
-      className="w-full border border-gray-300"
-    >
-      <FormattingToolbarController
-        formattingToolbar={() => (
-          <FormattingToolbar>
-            <BlockTypeSelect key={"blockTypeSelect"} />
-
-            {/* Extra button to toggle blue text & background */}
-            <BlueButton key={"customButton"} />
-
-            <FileCaptionButton key={"fileCaptionButton"} />
-            <FileReplaceButton key={"replaceFileButton"} />
-
-            <BasicTextStyleButton
-              basicTextStyle={"bold"}
-              key={"boldStyleButton"}
-            />
-            <BasicTextStyleButton
-              basicTextStyle={"italic"}
-              key={"italicStyleButton"}
-            />
-            <BasicTextStyleButton
-              basicTextStyle={"underline"}
-              key={"underlineStyleButton"}
-            />
-            <BasicTextStyleButton
-              basicTextStyle={"strike"}
-              key={"strikeStyleButton"}
-            />
-            {/* Extra button to toggle code styles */}
-            <BasicTextStyleButton
-              key={"codeStyleButton"}
-              basicTextStyle={"code"}
-            />
-
-            <TextAlignButton
-              textAlignment={"left"}
-              key={"textAlignLeftButton"}
-            />
-            <TextAlignButton
-              textAlignment={"center"}
-              key={"textAlignCenterButton"}
-            />
-            <TextAlignButton
-              textAlignment={"right"}
-              key={"textAlignRightButton"}
-            />
-
-            <ColorStyleButton key={"colorStyleButton"} />
-
-            <NestBlockButton key={"nestBlockButton"} />
-            <UnnestBlockButton key={"unnestBlockButton"} />
-
-            <CreateLinkButton key={"createLinkButton"} />
-          </FormattingToolbar>
-        )}
-      />
-    </BlockNoteView>
-  );
+interface ToolbarProps {
+  value: string;
+  onChange: (value: string) => void;
 }
+
+const Toolbar: React.FC<ToolbarProps> = ({ value, onChange }) => {
+  const quillRef = useRef<ReactQuill | null>(null);
+  const toolbarOptions = [
+    ["bold", "italic", "underline"],
+    [
+      { align: "" },
+      { align: "center" },
+      { align: "right" },
+      { align: "justify" },
+    ],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["clean"],
+  ];
+
+  const modules = {
+    toolbar: toolbarOptions,
+  };
+
+  return (
+    <div>
+      <ReactQuill
+        ref={quillRef}
+        modules={modules}
+        theme="snow"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Toolbar;
--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,46 +1,58 @@
 export type signupDataType = {
-    email : string,
-    password : string,
+  email: string;
+  password: string;
 };
 
 export type signinDataType = {
-    email : string,
-    password : string,
-}
+  email: string;
+  password: string;
+};
 
 export type userDataType = {
-    email : string,
-    phone?: string | "",
-    bio: string | "",
-    name: string | "",
-    profileImg: string | "",
-    gender: string | "",
-}
+  email: string;
+  phone?: string | "";
+  bio: string | "";
+  name: string | "";
+  profileImg: string | "";
+  gender: string | "";
+};
 
 export type User = {
-    email : string,
-    password : string,
-    phone?: string | "",
-    bio: string | "",
-    name: string | "",
-    profileImg: string | "",
-    gender: string | "",
-}
+  email: string;
+  password: string;
+  phone?: string | "";
+  bio: string | "";
+  name: string | "";
+  profileImg: string | "";
+  gender: string | "";
+};
 
 export type categoryType = {
-    map(arg0: ({ id, title, icon }: { id: number; title: string; icon: string; }) => import("react/jsx-runtime").JSX.Element): unknown;
-    length: number;
-    id: number,
-    title : string,
-    icon : string
-}
+  map(
+    arg0: ({
+      id,
+      title,
+      icon,
+      priority,
+    }: {
+      id: number;
+      title: string;
+      icon: string;
+      priority: string;
+    }) => import("react/jsx-runtime").JSX.Element
+  ): any;
+  length: number;
+  id: number;
+  title: string;
+  icon: string;
+  priority: string;
+};
 
-
-export type  BookDataType = {
-    title : string,
-    coverImg : File | string,
-    description : string,
-    categoryId : string,
-    status : string,
-    keywords : string[],
-}
+export type BookDataType = {
+  title: string;
+  coverImg: File | string;
+  description: string;
+  categoryId: string;
+  status: string;
+  keywords: string[];
+};
