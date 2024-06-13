f02584624162522b9918c6a73454c915c4308338
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2024-06-13 15:16:09 +0700

    update edit button

--- src/components/Loading/CategoryLoading.tsx
+++ src/components/Loading/CategoryLoading.tsx
@@ -5,35 +5,34 @@ import Skeleton from "@mui/material/Skeleton";
 const CategoryLoading = () => {
   return (
     <div className="mx-20 ">
-        <Grid container wrap="nowrap">
-      {Array.from({ length: 3 }, (_, i) => (
-        <div>
-          <Box key={i} sx={{ width: 350, marginRight: 2, marginBottom: 5 }}>
-            <Skeleton variant="rectangular" height={50} />
-          </Box>
-        </div>
-      ))}
-    </Grid>
-    <Grid container wrap="nowrap">
-      {Array.from({ length: 3 }, (_, i) => (
-        <div>
-          <Box key={i} sx={{ width: 350, marginRight: 2, marginBottom: 5 }}>
-            <Skeleton variant="rectangular" height={50} />
-          </Box>
-        </div>
-      ))}
-    </Grid>
-    <Grid container wrap="nowrap">
-      {Array.from({ length: 3 }, (_, i) => (
-        <div>
-          <Box key={i} sx={{ width: 350, marginRight: 2, marginBottom: 5 }}>
-            <Skeleton variant="rectangular" height={50} />
-          </Box>
-        </div>
-      ))}
-    </Grid>
+      <Grid container wrap="nowrap">
+        {Array.from({ length: 3 }, (_, i) => (
+          <div key={i}>
+            <Box sx={{ width: 350, marginRight: 2, marginBottom: 5 }}>
+              <Skeleton variant="rectangular" height={50} />
+            </Box>
+          </div>
+        ))}
+      </Grid>
+      <Grid container wrap="nowrap">
+        {Array.from({ length: 3 }, (_, i) => (
+          <div key={i}>
+            <Box sx={{ width: 350, marginRight: 2, marginBottom: 5 }}>
+              <Skeleton variant="rectangular" height={50} />
+            </Box>
+          </div>
+        ))}
+      </Grid>
+      <Grid container wrap="nowrap">
+        {Array.from({ length: 3 }, (_, i) => (
+          <div key={i}>
+            <Box sx={{ width: 350, marginRight: 2, marginBottom: 5 }}>
+              <Skeleton variant="rectangular" height={50} />
+            </Box>
+          </div>
+        ))}
+      </Grid>
     </div>
-    
   );
 };
 
